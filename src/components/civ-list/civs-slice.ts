30f3ab812ea835fb6ac8362c1fe6c74dc4f43ae2
Amr Gaber <amrtgaber@protonmail.com>
2022-08-06 16:21:10 -0700

    add civs slice
    the component does not use the slice yet and the thunk/api calls are not implemented yet

---
+++ src/components/civ-list/civs-slice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../store';
+
+export interface CivsState {
+  list: string[];
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: CivsState = {
+  list: [],
+  status: 'idle',
+};
+
+export const getCivs = createAsyncThunk('civs/fetch', async () => {
+  // @TODO: implement
+  // const response = await fetch(`${apiUrl}/civs`);
+  // const civs = await response.json();
+  return [];
+});
+
+export const civsSlice = createSlice({
+  name: 'civs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCivs.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getCivs.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.list = action.payload;
+      })
+      .addCase(getCivs.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
+});
+
+export const {} = civsSlice.actions;
+
+export const selectCivs = (state: RootState) => state.civs.list;
+
+export default civsSlice.reducer;
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import civsReducer from '../components/civ-list/civs-slice';
+
+export const store = configureStore({
+  reducer: {
+    civs: civsReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
