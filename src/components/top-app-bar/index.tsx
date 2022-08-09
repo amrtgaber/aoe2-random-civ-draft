3e8e1e50c1abbe69900ddc0f9b49cec3b8255e62
Amr Gaber <amrtgaber@protonmail.com>
2022-08-08 23:06:19 -0700

    update logo
    to reflect the new name of the app

--- src/components/top-app-bar/index.tsx
+++ src/components/top-app-bar/index.tsx
@@ -8,7 +8,7 @@ const TopAppBar: FC<ITopAppBarProps> = (props) => {
   return (
     <div className='top-app-bar'>
       <a className='logo' href='/'>
-        AoE<span className='numeral'>II</span> Random Civ Picker
+        AoE<span className='numeral'>II</span> Random Civ Draft
       </a>
     </div>
   );
