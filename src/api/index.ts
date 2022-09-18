0022bd09d98b2f9667499a3f6dc2a76ad5808164
LimitToInfinity <21stLimitless@gmail.com>
2022-09-18 16:58:05 -0600

    Resets API url for prod.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://aoe2-data-api.herokuapp.com';
-export const API_URL = 'http://localhost:4000'; // dev
+export const API_URL = 'https://aoe2-data-api.herokuapp.com';
+// export const API_URL = 'http://localhost:4000'; // dev
