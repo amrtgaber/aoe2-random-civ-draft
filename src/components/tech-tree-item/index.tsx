92f58eb4193e3545c6cdfbe80bbaafe64ee7c52a
Amr Gaber <amrtgaber@protonmail.com>
2022-09-21 20:15:09 -0700

    WIP tech-tree-item icons

--- src/components/tech-tree-item/index.tsx
+++ src/components/tech-tree-item/index.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
 
 import './tech-tree-item.scss';
-import { ITechTreeItem } from '../../api/tech-tree-item-api';
+import { isTech, isUnit, ITechTreeItem } from '../../api/tech-tree-item-api';
 import { useAppDispatch } from '../../hooks';
 import {
   addItemToFilter,
@@ -22,9 +22,17 @@ export const TechTreeItem: FC<ITechTreeItemProps> = (props) => {
 
   const kind = item.kind;
   const name = item.itemName;
+  const age = item.age!;
   const imgSrc = `/assets/images/tech-tree/${kind}s/${name}.png`;
   const uniqueClass = item.isUnique ? 'unique' : '';
   const enterClass = selected ? 'enter-selected' : 'enter-unselected';
+
+  const ageIcons: { [key: string]: string } = {
+    ['dark age']: '/assets/images/tech-tree/age-icons/dark_age.png',
+    ['feudal age']: '/assets/images/tech-tree/age-icons/feudal_age.png',
+    ['castle age']: '/assets/images/tech-tree/age-icons/castle_age.png',
+    ['imperial age']: '/assets/images/tech-tree/age-icons/imperial_age.png',
+  };
 
   const handleDeselectItem = () => {
     setLeaveClass('leave-selected');
@@ -44,6 +52,23 @@ export const TechTreeItem: FC<ITechTreeItemProps> = (props) => {
     }
   };
 
+  const renderBuildingIcons = (item: ITechTreeItem): JSX.Element[] => {
+    const imgBase = '/assets/images/tech-tree/buildings/';
+
+    if (isUnit(item) || isTech(item)) {
+      return item.buildings.map((building) => (
+        <img
+          key={building.id}
+          src={`${imgBase}/${building.itemName}.png`}
+          className='tech-tree-item-building-icon'
+          alt={`${building.itemName}`}
+        />
+      ));
+    } else {
+      return [];
+    }
+  };
+
   return (
     <div className='tech-tree-item-container'>
       <div
@@ -51,7 +76,17 @@ export const TechTreeItem: FC<ITechTreeItemProps> = (props) => {
         onClick={selected ? handleDeselectItem : handleSelectItem}
         onTransitionEnd={handleTransitionEnd}
       >
-        <img src={imgSrc} />
+        <div className='tech-tree-item-icons-container'>
+          <div className='tech-tree-item-icons'>
+            {renderBuildingIcons(item)}
+            <img
+              src={ageIcons[age.ageName]}
+              className='tech-tree-item-age-icon'
+              alt={`${name} age`}
+            />
+          </div>
+        </div>
+        <img src={imgSrc} className='tech-tree-item-img' alt={`${name}`} />
         <div className='tech-tree-item-name'>{name}</div>
       </div>
     </div>
