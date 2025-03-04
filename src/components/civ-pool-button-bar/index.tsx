f71b0d55c631d44cb4fdc236cbeb8afca59517ba
Amr Gaber <amrtgaber@protonmail.com>
2025-03-04 15:17:59 -0700

    add primary button bar buttons

--- src/components/civ-pool-button-bar/index.tsx
+++ src/components/civ-pool-button-bar/index.tsx
@@ -30,18 +30,21 @@ export const CivPoolButtonBar: FC<ICivPoolButtonBarProps> = (props) => {
   };
 
   return (
-    <>
-      <div className='civ-pool-button-bar civ-pool-buttons'>
-        <a className='reset-pool-button' onClick={handleReset}>
-          Reset
-        </a>
-        <a className='add-all-civs-button' onClick={handleAddAllCivs}>
-          Add all civs
-        </a>
-        <a className='invert-pool-button' onClick={handleInvertPool}>
-          Invert selection
-        </a>
-      </div>
-    </>
+    <div className='civ-pool-button-bar civ-pool-buttons'>
+      <button className='reset-pool-button' onClick={handleReset}>
+        Reset
+      </button>
+      <button className='add-all-civs-button' onClick={handleAddAllCivs}>
+        Add all civs
+      </button>
+      <button className='invert-pool-button' onClick={handleInvertPool}>
+        Invert selection
+      </button>
+      <div className='civ-pool-button-bar-pad' />
+      <button className='save-new-pool-button'>Save as new</button>
+      <button className='save-pool-button'>Save</button>
+      <button className='like-pool-button'>Like</button>
+      <button className='share-pool-button'>Share</button>
+    </div>
   );
 };
