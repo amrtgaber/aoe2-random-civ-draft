ca237c79711e4838b0e4741dbdbcd062eb710d20
LimitToInfinity <21stLimitless@gmail.com>
2025-03-05 17:50:19 -0700

    Load draft name and description as text fields.

--- src/components/civ-pool-container/index.tsx
+++ src/components/civ-pool-container/index.tsx
@@ -5,11 +5,13 @@ import { CivPool } from '../civ-pool';
 import { CivPoolButtonBar } from '../civ-pool-button-bar';
 import { useAppSelector } from '../../hooks';
 import { selectCivs } from '../../store/slices/civs-slice';
+import { selectDrafts } from '../../store/slices/drafts-slice';
 
 export interface ICivPoolContainerProps {}
 
 export const CivPoolContainer: FC<ICivPoolContainerProps> = (props) => {
-  const { allCivs, civPool, civsStatus } = useAppSelector(selectCivs);
+  const { allCivs, civPool } = useAppSelector(selectCivs);
+  const { draft } = useAppSelector(selectDrafts);
   const civPoolCount = civPool.length > 0 ? civPool.length : allCivs.length;
 
   return (
@@ -23,6 +25,12 @@ export const CivPoolContainer: FC<ICivPoolContainerProps> = (props) => {
         {civPoolCount !== 1 && 's'} in current draft pool
       </p>
       <div className='civ-pool-container'>
+        {draft && (
+          <div className='draft-name-container'>
+            <h2 className='draft-name'>{draft?.name}</h2>
+            <p className='draft-description'>{draft?.desc}</p>
+          </div>
+        )}
         <CivPool />
         <CivPoolButtonBar />
       </div>
